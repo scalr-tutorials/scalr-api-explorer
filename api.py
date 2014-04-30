f3e9ec5fc7f9b305ad7c5137e0e1b2e3be5277af
Thomas Orozco <thomas@orozco.fr>
2014-04-30 13:23:56 -0400

    Support methods with no arguments (^ ^)

--- api.py
+++ api.py
@@ -3,4 +3,9 @@ import os
 import json
 
 STRUCTURE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "api-structure.json")
-API_STRUCTURE = json.load(open(STRUCTURE_FILE))
+API_STRUCTURE = {}
+
+for api_method, method_params in json.load(open(STRUCTURE_FILE)).items():
+    if not method_params:
+        method_params = {}
+    API_STRUCTURE[api_method] = method_params
